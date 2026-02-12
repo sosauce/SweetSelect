3682e8a120fc07058598521ad2f3e1299c75fdb6
sosauce <98750531+sosauce@users.noreply.github.com>
2026-02-12 23:20:55 +0100

    changes for jipack

--- SweetSelect/build.gradle.kts
+++ SweetSelect/build.gradle.kts
@@ -6,36 +6,25 @@ plugins {
 
 android {
     namespace = "com.sosauce.sweetselect.compose"
-    compileSdk {
-        version = release(36)
-    }
+    compileSdk = 36
 
     defaultConfig {
         minSdk = 24
-
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         consumerProguardFiles("consumer-rules.pro")
     }
 
     publishing {
         singleVariant("release") {
+            withSourcesJar()
         }
     }
 
-    buildTypes {
-        release {
-            isMinifyEnabled = true
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
 
     buildFeatures {
         compose = true
@@ -45,17 +34,14 @@ android {
 group = "com.github.sosauce"
 version = "1.0.0"
 
-dependencies {
-    implementation(platform(libs.androidx.compose.bom))
-    implementation(libs.androidx.ui)
-    implementation(libs.androidx.compose.foundation)
-}
-
-publishing {
-    publications {
-        register<MavenPublication>("release") {
-            afterEvaluate {
+afterEvaluate {
+    publishing {
+        publications {
+            register<MavenPublication>("release") {
                 from(components["release"])
+
+                groupId = "com.github.sosauce"
+                artifactId = "sweetselect"
             }
         }
     }
