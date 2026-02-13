984f2dad45e3178110bdc0119b78eb8244ddfdc6
sosauce <98750531+sosauce@users.noreply.github.com>
2026-02-13 08:19:38 +0100

    add maven publishing support

--- SweetSelect/build.gradle.kts
+++ SweetSelect/build.gradle.kts
@@ -1,8 +1,11 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.compose.compiler)
-    id("maven-publish")
-}
+    id("com.vanniktech.maven.publish") version "0.36.0"}
+
+
+group = "io.github.sosauce"
+version = "1.0.0"
 
 android {
     namespace = "com.sosauce.sweetselect.compose"
@@ -33,18 +36,37 @@ android {
     }
 }
 
-group = "com.github.sosauce"
-version = "1.0.0"
+mavenPublishing {
+    publishToMavenCentral()
+    signAllPublications()
+}
 
-afterEvaluate {
-    publishing {
-        publications {
-            register<MavenPublication>("release") {
-                from(components["release"])
+mavenPublishing {
+    coordinates(group.toString(), "sweetselect-compose", version.toString())
 
-                groupId = "com.github.sosauce"
-                artifactId = "sweetselect"
+    pom {
+        name.set("Sweet Select")
+        description.set("A Compose library that makes multi-selection really easy and highly optimized, with support for a finite amount of selectable elements!")
+        inceptionYear.set("2026")
+        url.set("https://github.com/sosauce/sweetselect/")
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
             }
+        }
+        developers {
+            developer {
+                id.set("sosauce")
+                name.set("sosauce")
+                url.set("https://github.com/sosauce/")
+            }
+        }
+        scm {
+            url.set("https://github.com/sosauce/sweetselect/")
+            connection.set("scm:git:git://github.com/sosauce/sweetselect.git")
+            developerConnection.set("scm:git:ssh://git@github.com/sosauce/sweetselect.git")
         }
     }
 }
